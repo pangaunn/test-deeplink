d627813211b1de2ac93a068628d7692a4528c002
Pangaunn <aunn_it08@hotmail.com>
2021-02-07 17:17:03 +0700

    feat: add youtube, spotify and applemusic

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Deeplink from '../views/Deeplink.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/:pathMatch(.*)*', name: 'wildcard', component: Deeplink }
 ]
 
 const router = createRouter({
---
+++ src/views/Deeplink.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>yoooo</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default {
+  name: 'Deeplink',
+  props: {
+    msg: String
+  },
+  setup () {
+    const route = useRoute()
+    onMounted(() => {
+      const { type } = route.query
+      switch (type) {
+        case 'youtube':
+          window.location = 'youtube://youtube.com/playlist?list=PLYd7ec6V3xTNCfQK7-ceEImpWUKUQ4RjC'
+          break
+        case 'spotify':
+          window.location = 'spotify://playlist/37i9dQZF1DXdzw50tzi795'
+          break
+        case 'applemusic':
+          window.location = 'intent://music.apple.com/us/playlist/soul-brother-no-2/pl.7927da5af7904fd18d1cadc916461e15/#Intent;package=com.apple.android.music;scheme=https;end'
+          break
+        default:
+      }
+
+      setTimeout(() => {
+        window.close()
+      }, 500)
+    })
+  }
+}
+</script>
